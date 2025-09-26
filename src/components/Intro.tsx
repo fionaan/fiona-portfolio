80ba786e05ac5b4de8ddbf2b6041b0985d19fd83
fiona <fionaanleimariano@gmail.com>
2025-09-26 23:51:57 +0800

    initial commit

---
+++ src/components/Intro.tsx
@@ -0,0 +1,10 @@
+export default function Intro() {
+  return (
+    <>
+      <article className="flex flex-column justify-center bg-red-100 h-screen">
+        <h1 className="text-transparent bg-clip-text bg-radial from-[rgb(255,0,212)] from-50% to-[rgb(183,0,255)]">Fiona</h1>
+      </article>
+    </>
+  )
+}
+
