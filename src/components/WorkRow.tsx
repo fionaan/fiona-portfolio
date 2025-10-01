1dccc48775fa85cadaa0d57fb450600520e9c80b
fionaan <fionaanleimariano@gmail.com>
2025-10-02 00:28:08 +0800

    work row layout

--- src/components/WorkRow.tsx
+++ src/components/WorkRow.tsx
@@ -1,7 +1,9 @@
-export default function WorkRow () {
-    return (
-        <div className="border-t-1 ">
+import type { workType } from "./Works";
 
+export default function WorkRow ({title}: workType) {
+    return (
+        <div className="flex flex-col justify-end border-b-1 border-b-white/20 mt-7 h-[22vmin] pb-3">
+            <p className="work-title text-white w-[70%]">{title}</p>
         </div>
     )
 }
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,9 +1,21 @@
 import SectionTitle from "./SectionTitle";
+import WorkRow from "./WorkRow";
+
+export type workType = {
+    title: string,
+}
 
 export default function Works () {
+    const works : workType[] = [
+        {title: "Airline Booking Reservation System"},
+        {title: "Electronic Medical Record (EMR) System"},
+    ]
     return (
         <div className="h-screen px-20">
             <SectionTitle title="Works"/>
+            {works.map(w => (
+                <WorkRow title={w.title}/>
+            ))}
         </div>
     )
 }
