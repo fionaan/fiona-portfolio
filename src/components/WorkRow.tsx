982686ca7d7a6ed41d0f9cfe7fee2e68023aeac9
fionaan <fionaanleimariano@gmail.com>
2025-10-03 22:32:05 +0800

    added image types and data

--- src/components/WorkRow.tsx
+++ src/components/WorkRow.tsx
@@ -2,16 +2,17 @@ import { motion } from "motion/react";
 import type { workType } from "./Works";
 
 
-export default function WorkRow ({title, description}: workType) {
+export default function WorkRow ({title, description, image}: workType) {
     return (
         <motion.div
             whileHover={{ 
-                height: {{24vmin}}
+                scale: 1.1,
             }}
 
             className="flex flex-col justify-end gap-y-2 border-b-1 border-b-white/20 mt-7 h-[22vmin] pb-3">
             <p className="work-title text-transparent bg-clip-text bg-linear-to-r from-white to-newBlack from-60% w-[60%] overflow-hidden whitespace-nowrap">{title}</p>
             <p className="work-description w-[50%] overflow-hidden text-ellipsis max-h-15">{description}</p>
+            <img src="" alt="" />
         </motion.div>
     )
 }
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -4,6 +4,12 @@ import WorkRow from "./WorkRow";
 export type workType = {
     title: string,
     description: string,
+    img: imageType,
+}
+
+export type imageType = {
+    src: string,
+    alt: string,
 }
 
 type workTypeArray =  workType[];
@@ -13,10 +19,18 @@ export default function Works () {
         {
             title: "MERC Airlines",
             description: "If you're using Save As cairo png, you will get a white background.",
+            img: {
+                src: "",
+                alt: ""
+            }
         },
         {
             title: "CentroTech",
             description: "If you're using Save As cairo png, you will get a white background. The proper way to make a PNG with transparent background is to use Export Bitmap, as TechieLog said.",
+            img: {
+                src: "",
+                alt: ""
+            }
         },
     ]
     return (
